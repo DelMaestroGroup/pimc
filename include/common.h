836a806d45f22d929df080275548e16fcc144b84
Nathan S. Nichols <8723338+nscottnichols@users.noreply.github.com>
2025-03-01 23:40:52 +0000

    add overload to print iVec, dVec, etc.

--- include/common.h
+++ include/common.h
@@ -235,4 +235,18 @@ constexpr std::array<T, N> make_array(const T& value) {
     return make_array_impl<T, N>(value, std::make_index_sequence<N>{});
 }
 
+// Templated overload for printing any std::array<T, N>
+template<typename T, std::size_t N>
+std::ostream& operator<<(std::ostream& os, const std::array<T, N>& arr) {
+    os << "(";
+    for (std::size_t i = 0; i < N; ++i) {
+        os << arr[i];
+        if (i < N - 1) {
+            os << ", ";
+        }
+    }
+    os << ")";
+    return os;
+}
+
 #endif
